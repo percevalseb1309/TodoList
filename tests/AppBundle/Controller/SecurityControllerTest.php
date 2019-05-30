5c4d2f45d6c63f84d790d8950f5b7798b14b9eca
percevalseb1309 <percevalseb@gmail.com>
2019-05-30 15:21:27 +0200

    functional tests on the security controller #5

---
+++ tests/AppBundle/Controller/SecurityControllerTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use Symfony\Component\HTTPFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityControllerTest extends WebTestCase
+{
+    private $client = null;
+    private $crawler = null;
+
+    protected function setUp()
+    {
+        $this->client = static::createClient();
+        $this->crawler = $this->client->request('GET', '/login');
+    }
+
+    public function loginAction()
+    {
+        static::assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+        static::assertContains('/login_check', $this->crawler->filter('form')->attr('action'));
+    }
+
+    public function testLoginActionWithValidUserData()
+    {
+        $form = $this->crawler->selectButton('Se connecter')->form();
+        $form['_username'] = 'John-Doe';
+        $form['_password'] = 'azerty';
+        $this->client->submit($form);
+        static::assertSame(Response::HTTP_FOUND, $this->client->getResponse()->getStatusCode());
+
+        $this->crawler = $this->client->followRedirect();
+        static::assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+        static::assertContains('Bienvenue sur Todo List', $this->crawler->filter('h1')->text());
+
+        $link = $this->crawler->selectLink('Se déconnecter')->link();
+        $this->crawler = $this->client->click($link);
+        static::assertSame(Response::HTTP_FOUND, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testLoginActionWithValidAdminData()
+    {
+        $form = $this->crawler->selectButton('Se connecter')->form();
+        $form['_username'] = 'Percevalseb';
+        $form['_password'] = 'azerty';
+        $this->client->submit($form);
+        static::assertSame(Response::HTTP_FOUND, $this->client->getResponse()->getStatusCode());
+
+        $this->crawler = $this->client->followRedirect();
+        static::assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+        static::assertContains('Bienvenue sur Todo List', $this->crawler->filter('h1')->text());
+
+        $link = $this->crawler->selectLink('Consulter la liste des utilisateurs')->link();
+        $this->crawler = $this->client->click($link);
+        static::assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+        static::assertContains('Liste des utilisateurs', $this->crawler->filter('h1')->text());
+    }
+
+    public function testLoginActionWithInvalidData()
+    {
+        $form = $this->crawler->selectButton('Se connecter')->form();
+        $form['_username'] = 'xxx';
+        $form['_password'] = 'xxx';
+        $this->client->submit($form);
+        static::assertSame(Response::HTTP_FOUND, $this->client->getResponse()->getStatusCode());
+
+        $this->crawler = $this->client->followRedirect();
+        static::assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+        static::assertContains("Invalid credentials.", $this->crawler->filter('div.alert-danger')->text());
+    }
+}
